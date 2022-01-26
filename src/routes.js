e965df29ebd62647430a1a7f5b47c8247fcf8179
MarcusDias-r <marcusdias22@hotmail.com>
2022-01-25 21:35:03 -0300

    second commit

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
+const UserController = require("./controllers/UserController")
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ hello: 'World'});
-})
-
-// routes.post('/users', )
+// routes.get('/', (req, res) => {
+//     return res.json({ hello: 'World'});
+// })
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
 
 module.exports = routes;
